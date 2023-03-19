4b61466019ea52fc01647b0fb24586b3ba98d3d0
Vaiwasvat2308 <vaiwasvatyeo25@gmail.com>
2023-03-19 12:50:28 +0530

    demo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const newVar=async(req,res) => {
-    res.await.write("Hello,!");
+    res.await.write("Hello,nanded!");
     res.end();
 };
 http.createServer(newVar).listen(2000)
