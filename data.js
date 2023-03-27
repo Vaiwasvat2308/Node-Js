ced0e7298d3eb485b1fdcc7c778965bde14f3e00
Vaiwasvat2308 <vaiwasvatyeo25@gmail.com>
2023-03-27 14:19:59 +0530

    chore: new directory

---
+++ data.js
@@ -0,0 +1,6 @@
+const data=[
+    {name:"vaiwasvat",email:"vaiwasvaty@gmail.com"},
+    {name:"rush",email:"rush@gmail.com"},
+    {name:"serjhhjt",email:"nkdnk@gmail.com"},
+]
+module.exports = data;
